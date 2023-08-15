2eaddd462acc6c56b4dba68bfe81b79edc92a969
Huylquias R. Santana <sshuylquias@gmail.com>
2023-08-15 20:53:30 -0300

    ta pronto

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-const form = document.getElementById(form)
-const numeroA = document.getElementById(numeroA)
-const numeroB = document.getElementById(numeroB)
+const form = document.getElementById('form-valid'); 
+
+function validarFormulario(valorA, valorB) { 
+    return valorB > valorA
+}
+
+form.addEventListener('submit', function(e){ 
+    let formValid
+    e.preventDefault(); 
+
+const valorA = document.getElementById('campoA').value; 
+const valorB = document.getElementById('campoB').value; 
+const menssagemSucesso = `É válido, o valor B: ${valorB} é maior que o valor A: ${valorA}`; 
+
+    formValid = validarFormulario(valorA, valorB)
+    if (formValid) { 
+        alert(menssagemSucesso);
+
+        valorA.value = '';
+        valorB.value = '';
+
+    } else if (valorB == valorA) { 
+        alert("Não é válido pois B é igual A!"); 
+    } else { 
+        alert("Não é válido pois A é maior que B!");
+    }
+})
+
+console.log(form);
